279a50443b8252dd3a91dd153425683d3198ee8a
OwenLobato <owenlobatov@gmail.com>
2022-11-28 01:05:27 -0600

    Add wordcloud image

--- app/blueprints/feeling.py
+++ app/blueprints/feeling.py
@@ -1,10 +1,15 @@
 """ FEELING Module """
 
+import nltk
+from nltk.corpus import stopwords
+from matplotlib.colors import ListedColormap
+from wordcloud import WordCloud
 from flask import render_template, Blueprint, flash, g, redirect, request, url_for
 from blueprints.auth import login_required
 from models.file_model import File
 from models.page_model import Page
 from helpers.excel_helper import ExcelHelper
+
 
 feeling_bp = Blueprint('feeling', __name__, url_prefix='/feelings/pages')
 
@@ -31,12 +36,44 @@ def index(page_id = None):
 @login_required
 def comments(file_id):
     """ Post comments sentiment analysis """
+    # File data
     (file_fetch, posts_fetch, comments_fetch) = ExcelHelper('').read_db_file(file_id)
+    # Word cloud
+    file = File().find_by_params({'id': file_id})
+    file_path = file.path + file.name
+    wordcloud_path = __get_word_cloud(file_path, file.name)
 
     return render_template(
         'feeling/comments.html',
         file_id = file_id,
         file_fetch = file_fetch,
         posts_fetch = posts_fetch,
-        comments_fetch = comments_fetch
+        comments_fetch = comments_fetch,
+        wordcloud_path = wordcloud_path
     )
+
+def __get_word_cloud(file_path, file_name):
+    # Download and set spanish stopwords
+    nltk.download('stopwords')
+    stop_words = set(stopwords.words('spanish'))
+    # Read and join all comments
+    comments = ExcelHelper(file_path).read_comments()
+    words = comments.message.tolist()
+    words = map(str, words)
+    words = ' '.join(words).lower()
+    # Generate wordcloud
+    word_cloud = WordCloud(
+        collocations = True,
+        background_color = '#FFFCF0',
+        colormap = ListedColormap(["#1F5C82", "#278ECF", "#CF3C3C", "#D5AC2E"]),
+        stopwords = stop_words,
+        width = 1500,
+        height = 400,
+        min_word_length = 5,
+        max_words = 100
+    ).generate(words)
+    # Save wordcloud to PNG image
+    file_name = file_name.split('.')[0]
+    wordcloud_path = f"uploads/{file_name}.png"
+    word_cloud.to_file(f"static/{wordcloud_path}")
+    return wordcloud_path
